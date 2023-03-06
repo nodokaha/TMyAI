64e5f831df110a3591d4c6c175b8a9f81cf2b478
nodokaha <scpeee@wail.ch>
2023-03-06 18:36:27 +0900

    mini update

--- conv.py
+++ conv.py
@@ -1,8 +1,19 @@
 import numpy
 import mpi4py
 
-def pooling
 
-def convolution
+def __init__:
+    filter = [[[1, 0, 0], [0, 1, 0], [0, 0, 1]], [[1, 0, 0], [1, 0, 0], [1, 0, 0]]]
+    
 
-def getdata
+def pooling(conv_out, pool_out):
+    for
+
+def iter_pooling(conv_out, i, j, POOLSIZE=3):
+    return conv_out[i+POOLSIZE/2][j+POOLSIZE]
+
+def convolution():
+
+
+def getdata():
+
